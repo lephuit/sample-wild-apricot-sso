7761df2ca52993ee4637029e18d398462b0c058d
matticustard <thomure.m@gmail.com>
2017-01-17 23:50:48 -0500

    Update wa-login.php

--- wa-login.php
+++ wa-login.php
@@ -2,27 +2,27 @@
 include_once 'includes/constants.php';
 
 // ** SET VARIABLES ------------------------
-$account_id = ACCOUNT_ID;
-$client_id = CLIENT_ID;
-$processing_path = "https://".PROCESSING_PATH;
-$auth_domain = "https://".WILD_APRICOT_DOMAIN;
+$account_id 		= ACCOUNT_ID;
+$client_id 		= CLIENT_ID;
+$processing_path 	= "https://".PROCESSING_PATH;
+$auth_domain 		= "https://".WILD_APRICOT_DOMAIN;
 
 // ** BUILD URLS ------------------------
-$redirect_uri = "{$processing_path}/wa-oauth.php";
-$accountUrl = "{$auth_domain}/sys/login/OAuthLogin";
+$redirect_uri 	= "{$processing_path}/wa-oauth.php";
+$accountUrl 	= "{$auth_domain}/sys/login/OAuthLogin";
 
 // ** END SET VARIABLES --------------------
 
 $state = $_SERVER['QUERY_STRING'];
 
 $queryParams = array(
-				'client_id' => $client_id, 
-				'redirect_uri' => $redirect_uri, 
-				'scope' => 'contacts_me',
-				'response_type' => 'authorization_code',
-				'claimed_account_id' => $account_id,
-				'state' => $state
-       		   );
+	'client_id' => $client_id, 
+	'redirect_uri' => $redirect_uri, 
+	'scope' => 'contacts_me',
+	'response_type' => 'authorization_code',
+	'claimed_account_id' => $account_id,
+	'state' => $state
+);
 
 $url = $accountUrl . '?' . http_build_query($queryParams);
 
