83fd5578c70c94d5315caff6412c44189483043b
matticustard <thomure.m@gmail.com>
2017-01-17 23:52:44 -0500

    Update wild_apricot_sso.php

--- includes/wild_apricot_sso.php
+++ includes/wild_apricot_sso.php
@@ -2,15 +2,15 @@
 include_once ("includes/wa_helper_class_sso.php");
 include_once ("includes/constants.php");
 
-$waApiClient = WaApiClient::getInstance();
+$waApiClient 		= WaApiClient::getInstance();
 
 // set API application credentials here
-$client_id = CLIENT_ID;
-$client_secret = CLIENT_SECRET;
-$processing_path = "https://".PROCESSING_PATH;
+$client_id 		= CLIENT_ID;
+$client_secret 		= CLIENT_SECRET;
+$processing_path 	= "https://".PROCESSING_PATH;
 
 // build redirect url
-$redirect_uri = "{$processing_path}/wa-oauth.php";
+$redirect_uri 		= "{$processing_path}/wa-oauth.php";
 
 // request access token
 try {
@@ -22,18 +22,18 @@ try {
 			
 	// get contact data
 	if( $contact = getContactsList() ) {
-		$_SESSION['first_name'] = $contact['FirstName'];
-		$_SESSION['last_name'] = $contact['LastName'];
-		$_SESSION['email'] = $contact['Email'];
-		$_SESSION['display_name'] = $contact['DisplayName'];
-		$_SESSION['organization_name'] = $contact['Organization'];
-		$_SESSION['membership_level'] = $contact['MembershipLevel']['Name'];
-		$_SESSION['membership_status'] = $contact['Status'];
-		$_SESSION['member_id'] = $contact['Id'];
-		$_SESSION['is_admin'] = $contact['IsAccountAdministrator'];
-		$_SESSION['tos_accepted'] = $contact['TermsOfUseAccepted'];
-		$_SESSION['WA_Login_Verified'] = true;
-		$_SESSION['timestamp'] = new DateTime();
+		$_SESSION['first_name'] 	= $contact['FirstName'];
+		$_SESSION['last_name'] 		= $contact['LastName'];
+		$_SESSION['email'] 		= $contact['Email'];
+		$_SESSION['display_name'] 	= $contact['DisplayName'];
+		$_SESSION['organization_name'] 	= $contact['Organization'];
+		$_SESSION['membership_level'] 	= $contact['MembershipLevel']['Name'];
+		$_SESSION['membership_status'] 	= $contact['Status'];
+		$_SESSION['member_id'] 		= $contact['Id'];
+		$_SESSION['is_admin'] 		= $contact['IsAccountAdministrator'];
+		$_SESSION['tos_accepted'] 	= $contact['TermsOfUseAccepted'];
+		$_SESSION['WA_Login_Verified'] 	= true;
+		$_SESSION['timestamp'] 		= new DateTime();
 	}
 	
 } catch (Exception $e) {
