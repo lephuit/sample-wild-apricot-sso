832b7af855d6c6eda3f77905acc47e7d22632381
matticustard <thomure.m@gmail.com>
2017-01-18 09:32:26 -0500

    Update wa-oauth.php

--- wa-oauth.php
+++ wa-oauth.php
@@ -2,32 +2,36 @@
 require_once ("includes/sec_session.php");
 sec_session_start();
 
-// get authorization code
 if (isset($_GET['code'])) {
-	$authorization_code = $_GET['code'];
-} else {
+
+	if (isset($_GET['state']) && $_GET['state'] != '') {	
+	
+		// get authorization code
+		$authorization_code = $_GET['code'];
+
+		// get destination and decode for redirect
+		$state = urldecode($_GET['state']);
+
+		// attempt single sign-on connection
+		require_once ("includes/wild_apricot_sso.php");	
+
+		// verify sign-on and redirect to destination
+		if ($_SESSION['WA_Login_Verified']) {
+			header("location: $state");
+		} else {			
+			// handle failure
+			session_destroy();
+			die("API request failed.");
+		}
+
+	} else {		
+		// handle failure
+		session_destroy();
+		die("No destination specified.");
+	}
+	
+} else {	
+	// handle failure
 	session_destroy();
 	die("Authorization code not found.");
-}
-
-// get destination
-if (isset($_GET['state']) && $_GET['state'] != '') {
-	$state = $_GET['state'];
-} else {
-	session_destroy();
-	die("No destination specified.");
-}
-
-// attempt single sign-on connection
-require_once ("includes/wild_apricot_sso.php");
-
-// decode destination
-$state = urldecode($state);
-
-// verify sign-on and redirect to destination
-if ($_SESSION['WA_Login_Verified']) {
-	header("location: $state");
-} else {
-	session_destroy();
-	die("API request failed.");
 }
