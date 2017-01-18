41879b665f239b4753ee8412e897b9e88b7b04cd
matticustard <thomure.m@gmail.com>
2017-01-18 00:31:01 -0500

    Update wa-login.php

--- wa-login.php
+++ wa-login.php
@@ -1,19 +1,13 @@
 <?
 include_once 'includes/constants.php';
 
-// ** SET VARIABLES ------------------------
 $account_id 		= ACCOUNT_ID;
 $client_id 		= CLIENT_ID;
 $processing_path 	= "https://".PROCESSING_PATH;
 $auth_domain 		= "https://".WILD_APRICOT_DOMAIN;
-
-// ** BUILD URLS ------------------------
-$redirect_uri 	= "{$processing_path}/wa-oauth.php";
-$accountUrl 	= "{$auth_domain}/sys/login/OAuthLogin";
-
-// ** END SET VARIABLES --------------------
-
-$state = $_SERVER['QUERY_STRING'];
+$redirect_uri 		= "{$processing_path}/wa-oauth.php";
+$accountUrl 		= "{$auth_domain}/sys/login/OAuthLogin";
+$state 			= urlencode($_SERVER['QUERY_STRING']);
 
 $queryParams = array(
 	'client_id' => $client_id, 
