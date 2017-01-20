948239aba6b7a11f656bc4d6e0d04c21e4c46578
matticustard <thomure.m@gmail.com>
2017-01-20 13:09:26 -0500

    Update constants.php

--- includes/constants.php
+++ includes/constants.php
@@ -2,5 +2,5 @@
 define('ACCOUNT_ID','{YOUR_ACCOUNT_ID}');                   // your Wild Apricot master account ID
 define('CLIENT_ID','{YOUR_CLIENT_ID}');                     // your API client id
 define('CLIENT_SECRET','{YOUR_CLIENT_SECRET}');             // your API client secret key
-define('PROCESSING_PATH','{YOUR_PROCESSING_PATH}');         // this is the location of sample-sso.php (e.g. example.com or example.com/sso_test)
+define('PROCESSING_PATH','{YOUR_PROCESSING_PATH}');         // this is the location of sample-sso.php (e.g. myserver.com or myserver.com/sso_test)
 define('WILD_APRICOT_DOMAIN','{YOUR_WILD_APRICOT_DOMAIN}'); // your wild apricot domain (e.g. mysite.com)
